7c025b14b3739f4c5eed9e8ca634d91952d2821b
stardream96 <stardream96@gmail.com>
2019-01-30 22:24:59 -0600

    dfs bfs finished
    dfs bfs finished

---
+++ search.py
@@ -0,0 +1,121 @@
+# search.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by Michael Abir (abir2@illinois.edu) on 08/28/2018
+# Modified by Rahul Kunji (rahulsk2@illinois.edu) on 01/16/2019
+
+"""
+This is the main entry point for MP1. You should only modify code
+within this file -- the unrevised staff files will be used for all other
+files and classes when code is run, so be careful to not modify anything else.
+"""
+
+
+# Search should return the path and the number of states explored.
+# The path should be a list of tuples in the form (row, col) that correspond
+# to the positions of the path taken by your search algorithm.
+# Number of states explored should be a number.
+# maze is a Maze object based on the maze from the file specified by input filename
+# searchMethod is the search method specified by --method flag (bfs,dfs,greedy,astar)
+from pygame.locals import *
+from agent import Agent
+from maze import Maze
+import time
+
+def search(maze, searchMethod):
+    return {
+        "bfs": bfs,
+        "dfs": dfs,
+        "greedy": greedy,
+        "astar": astar,
+    }.get(searchMethod)(maze)
+
+
+def bfs(maze):
+	startPoint = maze.getStart()
+	currentLevel=[startPoint]
+	finalPosition = maze.getObjectives()[-1]	
+	steps=0
+	path=[]
+	dict_parent={} #store the positions that is not chosed
+	explored=[] #record the positions explored
+	for position in currentLevel:
+		explored.append(position)
+	#time.sleep(0.5)
+	while (finalPosition not in currentLevel):
+		steps=steps+len(currentLevel)
+		#time.sleep(1)
+		nextLevel=[] #clear nextlevel data
+		for currentLevelposition in currentLevel: #iterate the positions in current level, and explore the next level
+			currentNeighbors=maze.getNeighbors(currentLevelposition[0], currentLevelposition[1])
+			for nextLevelPosition in currentNeighbors:
+				if nextLevelPosition not in explored: #not not the way that have been explored
+					nextLevel.append(nextLevelPosition)
+					dict_parent[nextLevelPosition]=currentLevelposition
+					explored.append(nextLevelPosition) #def the position as explored
+		currentLevel=nextLevel #update currentlevel
+		#print(currentLevel)
+	currentPosition=finalPosition
+	print(dict_parent,finalPosition)
+	while currentPosition != startPoint:
+		path.insert(0,currentPosition)
+		currentPosition=dict_parent[currentPosition]
+	path.insert(0,startPoint)
+	return path, steps
+
+	
+def dfs(maze):
+	currentPosition = maze.getStart()
+	finalPosition = maze.getObjectives()[-1]	
+	steps=0
+	path=[currentPosition]
+	dict_frontier={} #store the positions that is not chosed
+	while (currentPosition != finalPosition):
+
+		#time.sleep(0.1)
+		#print('position',currentPosition)
+		steps=steps+1
+		currentNeighbors = maze.getNeighbors(currentPosition[0], currentPosition[1])
+		#print('neighbors',currentNeighbors)
+		if currentPosition not in dict_frontier: #if not recorded in dict., record it
+			neighbors=[]
+			for neighbor in currentNeighbors:
+				if neighbor != currentPosition and neighbor not in dict_frontier: #not the way it comes from and not the way that have been explored
+					neighbors.append(neighbor)
+			#print(neighbors)
+			dict_frontier[currentPosition]=neighbors
+				#currentPosition=dict_frontier[currentPosition][0] #update the current position, chosen from the list of frontiers current position has
+
+		currentNeighbors = dict_frontier[currentPosition]
+		#print('poped',currentNeighbors)
+		if len(currentNeighbors) ==0: #if no more direction available, then wrong path, pop path items to retrieve, and update currentPosition
+			currentPosition=path.pop(-1)
+			if len(dict_frontier[currentPosition]) !=0:
+				path.append(currentPosition)
+		else: #if have direction available, go to the direction and pop it from the frontier dict.
+			lastPosition=currentPosition
+			#print(currentPosition,currentNeighbors)
+			currentPosition=currentNeighbors.pop(-1)
+			dict_frontier[lastPosition]=currentNeighbors
+			path.append(currentPosition) #add position to the path
+		#print('p',path)
+		if steps>2000:
+			#print(dict_frontier)
+			return path, steps
+	return path, steps
+
+
+def greedy(maze):
+    # TODO: Write your code here
+    # return path, num_states_explored
+    return [], 0
+
+
+def astar(maze):
+    # TODO: Write your code here
+    # return path, num_states_explored
+    return [], 0
